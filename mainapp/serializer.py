157e364e912104faf1a8a9a73bc30d96e1e84b63
tR1nZ <lesnikovvova111@yandex.ru>
2022-04-23 21:06:31 +0300

    first commit

---
+++ mainapp/serializer.py
@@ -0,0 +1,6 @@
+from rest_framework.serializers import HyperlinkedModelSerializer
+from .models import User
+class UserModelSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
